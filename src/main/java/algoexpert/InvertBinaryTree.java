2fb58517ff9a2a3914947e2d4eb6266badf6f616
Sai Mahendra Reddy <mahendra.reddy2111@gmail.com>
2020-09-22 11:24:26 +0530

    Invert binary tree problem

---
+++ src/main/java/algoexpert/InvertBinaryTree.java
@@ -0,0 +1,25 @@
+package algoexpert;
+
+public class InvertBinaryTree {
+    public static void invertBinaryTree(BinaryTree tree) {
+        // Write your code here.
+        if(tree == null){
+            return;
+        }
+        BinaryTree temp = tree.left;
+        tree.left = tree.right;
+        tree.right = temp;
+        invertBinaryTree(tree.left);
+        invertBinaryTree(tree.right);
+    }
+
+    static class BinaryTree {
+        public int value;
+        public BinaryTree left;
+        public BinaryTree right;
+
+        public BinaryTree(int value) {
+            this.value = value;
+        }
+    }
+}
